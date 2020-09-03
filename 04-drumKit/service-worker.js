ceafd6c1e22479fd0c8c524b7d1de29ed707059c
Tutul <tutulnahid@gmail.com>
2020-09-03 14:51:20 +0600

    drumKit: added style.css

--- 04-drumKit/service-worker.js
+++ 04-drumKit/service-worker.js
@@ -1,44 +1,47 @@
 const FILES_TO_CACHE = [
-  "/",
-  './index.html',
-  './app.js',
-  "./sounds/boom.wav",
-  "./sounds/clap.wav",
-  "./sounds/hihat.wav",
-  "./sounds/kick.wav",
-  "./sounds/openhat.wav",
-  "./sounds/ride.wav",
-  "./sounds/snare.wav",
-  "./sounds/tink.wav",
-  "./sounds/tom.wav",
-]
+    '/',
+    './index.html',
+    './styles.css',
+    './app.js',
+    './sounds/boom.wav',
+    './sounds/clap.wav',
+    './sounds/hihat.wav',
+    './sounds/kick.wav',
+    './sounds/openhat.wav',
+    './sounds/ride.wav',
+    './sounds/snare.wav',
+    './sounds/tink.wav',
+    './sounds/tom.wav',
+];
 //UPDATE VERSION WHEN U UPDATE CODE
-const CACHE_NAME = "STATIC_CACHE_V1"
+const CACHE_NAME = 'STATIC_CACHE_V1';
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      cache.addAll(FILES_TO_CACHE)
-    })
-  )
-})
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          caches.delete(key)
-        }
-      }))
-    })
-  )
-})
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(
+                keyList.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response ? response : fetch(event.request))
-  )
-})
-
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then((response) => (response ? response : fetch(event.request)))
+    );
+});
