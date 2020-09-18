d45f057b621d216c49252a2857806ea2222f32e5
Tutul <tutulnahid@gmail.com>
2020-09-18 22:20:45 +0600

    11-typeAhead added

--- 11-typeAhead/app.js
+++ 11-typeAhead/app.js
@@ -1,17 +1,70 @@
 const endpoint =
     'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
+const form = document.querySelector('.search-form');
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
+const defaultItems = suggestions.innerHTML;
+
 const cities = [];
+
+const numFormat = new Intl.NumberFormat('en-US');
+
+fetch(endpoint)
+    .then((response) => response.json())
+    .then((data) => cities.push(...data));
+
+///////////////////////////////
+// helped from https://tfrommen.github.io/JavaScript30/06%20-%20Type%20Ahead/
+function highlight(text, input) {
+    return text.replace(
+        new RegExp(input, 'gi'),
+        (match) => `<span class="hl">${match}</span>`
+    );
+}
+
+function itemizeCity({ city, state, population }, input) {
+    return `<li>
+                <span>${highlight(city, input)}, ${highlight(
+        state,
+        input
+    )}</span>
+                <span class="population">${numFormat.format(population)}</span>
+            </li>`;
+}
+
+// Find the matching words
+function matchesCity({ city, state }, input) {
+    const regex = new RegExp(input, 'i');
+
+    return regex.test(city) || regex.test(state);
+}
+
+function handleInput() {
+    const input = this.value;
+
+    if (input) {
+        suggestions.innerHTML = cities
+            .filter((city) => matchesCity(city, input))
+            .map((city) => itemizeCity(city, input))
+            .join('');
+
+        return;
+    }
+
+    suggestions.innerHTML = defaultItems;
+}
+
+form.addEventListener('submit', (e) => e.preventDefault());
+searchInput.addEventListener('input', handleInput);
+
+/*
+
+// WES' Solution
 
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
-
-fetch(endpoint)
-    .then((blob) => blob.json())
-    .then((data) => cities.push(...data));
 
 function findMatches(wordToMatch, cities) {
     return cities.filter((place) => {
@@ -21,7 +74,7 @@ function findMatches(wordToMatch, cities) {
     });
 }
 
-function displayMatches() {
+function handleInput() {
     // get the data from findMatches
     const matchArray = findMatches(this.value, cities);
 
@@ -49,8 +102,6 @@ function displayMatches() {
         .join('');
 
     suggestions.innerHTML = markup;
-    //suggestions.insertAdjacentHTML('beforeend', markup);
 }
 
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+*/
