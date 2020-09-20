bb68b2c01518b86dc225f906247c29bde2b65ad3
Tutul <tutulnahid@gmail.com>
2020-09-20 23:01:39 +0600

    12-drawing app

--- 12-drawing/app.js
+++ 12-drawing/app.js
@@ -1,52 +1,60 @@
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 
+// set the width & height
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-ctx.strokeStyle = '#bada55'; // start w/ this color
-ctx.lineJoin = 'round'; // when line joins its rounded
-ctx.lineCap = 'round'; // end of the line should be rounded
-ctx.lineWidth = 20; // size of the line
-//ctx.globalCompositeOperation = 'multiply'; // for blend mode
+ctx.strokeStyle = 'yellowgreen'; // starting color
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round'; // rounded line-end shape
+ctx.lineWidth = 25; // size of the brush
 
-let isDrawing = false; // draw only when the clicking is happening
-let lastX = 0;
-let lastY = 0; // points where to start drawing
-let hue = 0; // for rainbow like color
-let direction = true; // direction & lineWidth of the brush
+// Variables that'll work later
+let isDrawing = false;
+let [lastX, lastY] = [0, 0]; // starting points
+let hue = 0; // for colors
+let direction = true; // dir for the lineWidth
 
 function draw(e) {
-    if (!isDrawing) return; // stop the fn from running when they're not moused down
-    //console.log(e);
-
-    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    // stop the fn if mouse isn't down | otherwise it'll paint on mousemove
+    if (!isDrawing) return;
 
     ctx.beginPath();
     ctx.moveTo(lastX, lastY); // start from
     ctx.lineTo(e.offsetX, e.offsetY); // go to
     ctx.stroke();
 
-    [lastX, lastY] = [e.offsetX, e.offsetY]; // updating the start point
+    [lastX, lastY] = [e.offsetX, e.offsetY]; // start point are the same
 
+    // add some color variation
+    ctx.strokeStyle = `hsl(${hue}, 90%, 50%)`;
     hue++;
-    if (hue > 360) hue = 0;
+    if (hue >= 360) hue = 0;
 
-    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) direction = !direction; // flipping the dir
+    // flip the dir & lineWidth on condition
+    if (ctx.lineWidth > 50 || ctx.lineWidth < 10) direction = !direction;
 
-    // if the dir = true ++ , if = false --
-    if (direction) {
-        ctx.lineWidth++;
-    } else {
-        ctx.lineWidth--;
-    }
+    direction == true ? ctx.lineWidth++ : ctx.lineWidth--;
 }
 
+// Events
 canvas.addEventListener('mousedown', (e) => {
     isDrawing = true;
-    [lastX, lastY] = [e.offsetX, e.offsetY]; // updating the start point
+    [lastX, lastY] = [e.offsetX, e.offsetY];
 });
 
 canvas.addEventListener('mousemove', draw);
+
 canvas.addEventListener('mouseup', () => (isDrawing = false));
-canvas.addEventListener('mouseout', () => (isDrawing = false));
+canvas.addEventListener('mouseover', () => (isDrawing = false));
+
+/*
+// Couldn't figure out solution for mobile devices 😞
+canvas.addEventListener('touchstart', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('touchmove', draw);
+canvas.addEventListener('touchend', () => (isDrawing = false));
+*/
