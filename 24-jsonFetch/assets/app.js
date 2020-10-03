cb3b93e1391c218844fa45f73037af2f15bb5249
Tutul <tutulnahid@gmail.com>
2020-10-03 17:11:19 +0600

    updated jsonFetch & changed homepage

--- 24-jsonFetch/assets/app.js
+++ 24-jsonFetch/assets/app.js
@@ -1,7 +1,7 @@
 const tableBody = document.querySelector('.tbody');
 let items = [];
 
-fetch('projects.json')
+fetch('assets/projects.json')
     .then((response) => response.json())
     .then((data) => {
         items.push(...data);
@@ -15,20 +15,22 @@ function populateUI(arr = [], list) {
             <tr>
                 <td>${i < 10 ? '0' + i : i}</td>
                 <td>
-                    <a
-                        href="${el.urlLive}"
-                        target="_blank"
-                        >${el.name}</a
-                    >
+                    <a href="${el.urlLive}"
+                        target="_blank" >
+
+                        ${el.name}
+
+                    </a>
                 </td>
                 <td>
-                    <a
-                        href="${el.urlCode}"
-                        target="_blank"
-                        >${el.codeBase}</a
-                    >
+                    <a href="${el.urlCode}"
+                        target="_blank" >
+
+                        ${el.codeBase} 
+
+                    </a>
                 </td>
-                <td>${el.notes}</td>
+                <td> ${el.notes}</td>
             </tr>
         `;
         })
