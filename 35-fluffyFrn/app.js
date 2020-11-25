683577c5d9e377e4419419b41cf8351b25629b6b
TutulDevs <tutulnahid@gmail.com>
2020-11-26 00:52:34 +0600

    modified fluffy friends

--- 35-fluffyFrn/app.js
+++ 35-fluffyFrn/app.js
@@ -1,27 +1,47 @@
 const btns = document.querySelectorAll('.btn');
 const cards = document.querySelectorAll('.card');
 
+/*
+1. Store the clicked button's %data text in a variable.
+2. Loop through the cards/ boxes/ images.
+3. Store the image's %data text into  a variable.
+4. If the button's %data text matches the image's %data text or the %data text 'all', show those images. Otherwise hide them.
+*/
+
+// Function ES6
+
 function filter () {
-
-    // get the btn Text in case insensetive way
-    // this = button that got clicked
-    let btnName = this.innerText.toLowerCase();
-
     cards.forEach(card => {
-
-        // get the dataset, lowercase if needed
-        const tag = card.dataset.tag;
-
-        // store the tag & the all for all the el
-        const names = [tag, 'all'];
-
-        // IF btn name = names , show || hide
-        names.includes(btnName) ?
+        [card.dataset.tag, 'all'].includes(this.dataset.tag) ?
         card.style.display = 'grid':
         card.style.display = 'none';
-
-    }); // end of cards
+    });
 }
 
-// Event
+
+// Event    ES6
 btns.forEach(btn => btn.addEventListener('click', filter));
+
+
+/*
+// FN ES5
+function filter () {
+    const btnTag = this.dataset.tag;
+
+    for (let i = 0; i < cards.length; i++) {
+        cards[i];
+        const cardTag = cards[i].dataset.tag;
+
+        if (btnTag == cardTag || btnTag == 'all') {
+            cards[i].style.display = 'block';
+        } else {
+            cards[i].style.display = 'none';
+        }
+    }
+}
+
+// Event    ES5
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', filter);
+}
+*/
