2592fece15dcaa481bdccee857b38e1ca55ea523
TutulDevs <tutulnahid@gmail.com>
2020-12-08 23:09:11 +0600

    Weather&Clock v.10

--- 40-weatherClock/api.js
+++ 40-weatherClock/api.js
@@ -1,26 +1,5 @@
 const input = document.querySelector('.search');
 
-/*
-//  https://cors-anywhere.herokuapp.com/
-(function() {
-    var cors_api_host = 'cors-anywhere.herokuapp.com';
-    var cors_api_url = 'https://' + cors_api_host + '/';
-    var slice = [].slice;
-    var origin = window.location.protocol + '//' + window.location.host;
-    var open = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function() {
-        var args = slice.call(arguments);
-        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
-        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
-            targetOrigin[1] !== cors_api_host) {
-            args[1] = cors_api_url + args[1];
-        }
-        return open.apply(this, args);
-    };
-})();
-
-*/
-
 // API
 const key = 'mvi50abeoSc85sh0lVJWN14MGbmj50vs';
 
